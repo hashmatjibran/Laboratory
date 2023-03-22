5e15cd499b88d7f1036bf7c7c0db26fdc2c5aecc
hashmatjibran <hashmatjibran1@gmail.com>
2023-03-22 10:29:40 +0530

    Bug FIxed

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 8000;
+const router = express.Router();
 
-
+app.use(router);
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
-app.use('view engine','ejs');
-app.use('views','./ Views');
+app.set('view engine','ejs');
+app.set('views','./ Views');
 app.listen(PORT,(err)=>{
     if(err)
         {
         console.log(`Error in running the server Error: ${err}`);
         }
 });
+
+
+router.get('/', function (req, res, next) {
+    console.log("User Router Working");
+    res.end();
+});
+ 
