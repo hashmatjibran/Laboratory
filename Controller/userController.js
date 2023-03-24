e309d0315c2b86091afb4d78d204e9150f5f2453
hashmatjibran <hashmatjibran1@gmail.com>
2023-03-24 15:46:04 +0530

    sign Up

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,21 +2,31 @@ const User = require('../Models/userSchema');
 
 module.exports.signUp = async function (request , response) {
     try {
-        
-      await  User.create({
-        email:request.body.email,
-        password : request.body.password,
-        name:request.body.name,
-        age : request.body.age
-    });
 
-        return response.end("created Successfully");
+        // password check whether confirm password and password are equall
+        if(request.body.password != request.body.confirmPassword)
+        {
+            return response.end(`Password and Confirm Password Didn't Match!`);
+        }
+        
+        // try to create a user with given details from the user
+      await  User.create(request.body)
+      .then((result) => {
+        // if success, send this data back
+        return response.redirect('/signIn');
+        
+      }).catch((err) => {
+        if(err.code == '11000')
+        {
+            // if Email is Found in Db i.e Account with the emial is already associated so send the message below back to the user
+            return response.end(`Account with Email id: ${err.keyValue.email} Already exists! Try Using Another Email`);
+        }
+     });
+      
+        
 
     } catch (error) {
-        if(error.code == '11000')
-        {
-            return response.end(`Account with Email id: ${error.keyValue.email} Already exists! Try Using Another Email`)
-        }
+        
         return response.end(`Sign Up Failed Try Again Later! ERROR: ${error}`);
 
     }
@@ -40,7 +50,7 @@ module.exports.signIn = async function (request , response) {
             }
             if(result.password == request.body.password)
             {
-                return response.end(`Logged in successfully`);
+                return response.render('profile',{'name':result.name,'email':result.email});
             }
             else{
                 return response.end(`Incorrect Email/Password`);
