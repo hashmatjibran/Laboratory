e3481a3317025af2b90afc2ca9219eadd913c493
hashmatjibran <88573720+hashmatjibran@users.noreply.github.com>
2023-05-14 20:18:31 +0530

    deleted a post via postman request

--- Controller/api/v1/posts_api.js
+++ Controller/api/v1/posts_api.js
@@ -1,6 +1,62 @@
-module.exports.index = function (request , response) {
-        return response.status(200).json({
-            message:'list of Posts',
-            posts:[]
+
+const post = require('../../../Models/postsSchema');
+const comments = require('../../../Models/commentSchema');
+module.exports.index = async function (request , response) {
+
+    try {
+        
+        let posts = await post.find()
+    .sort('-createdAt')
+    .populate('user')
+     .populate({
+         path:'comments',
+         populate:{
+             path:'user'
+         }
+   
+     });
+
+
+     return response.status(200).json({
+            message: 'list of Posts',
+            post: posts
         });
+
+    } catch (error) {
+        return response.status(500).json({
+            message: 'internal server error'
+        });
+    }
+        
 }
+
+module.exports.delete = async function (request , response) { 
+
+    console.log(request.params.id);
+    try {
+
+        let findPost = await post.findById(request.params.id);
+
+    if(findPost)
+    {
+        let deletePost = await post.deleteOne({_id:request.params.id});
+        await comments.deleteMany({post:request.params.id});
+        return response.status(200).json({
+            message: "post and its associated comments deleted"
+        });
+    }else{
+        return response.status(404).json({
+            message: "post not found "
+        });
+    }
+
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({
+            message: "Internal server error"
+        });
+    }
+    
+
+
+ }
--- Routes/api/v1/posts.js
+++ Routes/api/v1/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const postApi = require('../../../Controller/api/v1/posts_api');
 
 router.get('/',postApi.index);
-
+router.delete('/:id',postApi.delete);
 
 module.exports = router;
